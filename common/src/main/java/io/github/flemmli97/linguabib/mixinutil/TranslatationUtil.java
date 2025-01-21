dddb75883571a4d9fab026f6df88219597d4c698
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-01-22 00:55:38 +0100

    rewrite player context fetching

--- common/src/main/java/io/github/flemmli97/linguabib/mixinutil/TranslatationUtil.java
+++ common/src/main/java/io/github/flemmli97/linguabib/mixinutil/TranslatationUtil.java
@@ -4,21 +4,38 @@ import io.github.flemmli97.linguabib.LinguaBib;
 import io.github.flemmli97.linguabib.Platform;
 import io.github.flemmli97.linguabib.api.LanguageAPI;
 import io.github.flemmli97.linguabib.data.ServerLangManager;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.AttributeKey;
+import net.minecraft.network.Connection;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 
 public class TranslatationUtil {
 
-    public static ServerPlayer context;
+    public static final AttributeKey<Connection> CONNECTION_ATTRIBUTE_KEY = AttributeKey.newInstance("linguabib:connection_ctx");
+    private static final ThreadLocal<ServerPlayer> CONTEXT = new ThreadLocal<>();
+
+    public static void handleEncode(ChannelHandlerContext ctx, boolean start) {
+        if (start) {
+            Connection listener = ctx.channel().attr(TranslatationUtil.CONNECTION_ATTRIBUTE_KEY).get();
+            if (listener != null && listener.getPacketListener() instanceof ServerGamePacketListenerImpl impl) {
+                CONTEXT.set(impl.getPlayer());
+            }
+        } else {
+            CONTEXT.remove();
+        }
+    }
 
     /**
      * Modifies the component if the player does not have the mod on the client
      * If the player has the mod on the client the client will handle translations
      */
     public static TranslatableContents modifyComponent(TranslatableContents orig) {
-        if (!LinguaBib.disableComponentMod && context != null && !Platform.INSTANCE.hasRemote(context)) {
+        ServerPlayer player = CONTEXT.get();
+        if (!LinguaBib.disableComponentMod && player != null && !Platform.INSTANCE.hasRemote(player)) {
             return new TranslatableContents(orig.getKey(),
-                    ServerLangManager.INSTANCE.getTranslationFor(LanguageAPI.getPlayerLanguage(context), orig.getKey()), orig.getArgs());
+                    ServerLangManager.INSTANCE.getTranslationFor(LanguageAPI.getPlayerLanguage(player), orig.getKey()), orig.getArgs());
         }
         return orig;
     }
