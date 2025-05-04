673f50d61c58037e2bdbc65d612a17756ae59c1e
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-05-04 18:07:14 +0200

    update/reorganize packet handling fabric

--- fabric/src/main/java/io/github/flemmli97/linguabib/fabric/client/LinguaBibFabricClient.java
+++ fabric/src/main/java/io/github/flemmli97/linguabib/fabric/client/LinguaBibFabricClient.java
@@ -1,12 +1,13 @@
 package io.github.flemmli97.linguabib.fabric.client;
 
-import io.github.flemmli97.linguabib.fabric.PacketRegister;
+import io.github.flemmli97.linguabib.network.S2CLangData;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 public class LinguaBibFabricClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        PacketRegister.register();
+        ClientPlayNetworking.registerGlobalReceiver(S2CLangData.TYPE, (pkt, ctx) -> S2CLangData.handle(pkt));
     }
 }
