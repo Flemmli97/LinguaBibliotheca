aa07fe6534d2f64763f153ece9405935816857a0
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-12-27 12:58:14 +0100

    update data listener registration fabric

--- fabric/src/main/java/io/github/flemmli97/linguabib/fabric/LinguaBibFabric.java
+++ fabric/src/main/java/io/github/flemmli97/linguabib/fabric/LinguaBibFabric.java
@@ -9,30 +9,14 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.v1.DataResourceLoader;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.PackType;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 public class LinguaBibFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(new IdentifiableResourceReloadListener() {
-            @Override
-            public ResourceLocation getFabricId() {
-                return ServerLangManager.ID;
-            }
-
-            @Override
-            public CompletableFuture<Void> reload(SharedState sharedState, Executor executor, PreparationBarrier preparationBarrier, Executor executor2) {
-                return ServerLangManager.INSTANCE.reload(sharedState, executor, preparationBarrier, executor2);
-            }
-        });
+        DataResourceLoader.get().registerReloader(ServerLangManager.ID, ServerLangManager.INSTANCE);
         PayloadTypeRegistry.playS2C().register(S2CLangData.TYPE, S2CLangData.STREAM_CODEC);
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> LinguaCommands.register(dispatcher));
         ServerPlayConnectionEvents.JOIN.register(((handler, sender, server) -> ServerLangManager.syncServerLangs(handler.player)));
